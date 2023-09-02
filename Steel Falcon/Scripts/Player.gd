b1c8dcefde931543b0b14aebbb4bc6b9d99417b8
David Goulet <18746130+davidgoulet@users.noreply.github.com>
2023-09-02 08:32:47 -0600

    Improved Character Movement

--- Steel Falcon/Scripts/Player.gd
+++ Steel Falcon/Scripts/Player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 @export var ActivePlayer = false
-@export var Speed = 300.0
+@export var Speed = 400.0
+@export var Acceleration = 800.0
 @export var JumpVelocity = -400.0
+@export var Friction = 1400.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -10,13 +12,22 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JumpVelocity
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = JumpVelocity
+	else:
+		$AnimatedSprite2D.play("Jumping")
+		if Input.is_action_just_released("jump") and velocity.y < JumpVelocity / 2:
+			velocity.y = JumpVelocity / 2
 
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * Speed
+		velocity.x = move_toward(velocity.x, Speed * direction, Acceleration * delta)
+		if is_on_floor():
+			$AnimatedSprite2D.play("Moving")
 	else:
-		velocity.x = move_toward(velocity.x, 0, Speed)
+		velocity.x = move_toward(velocity.x, 0, Friction * delta)
+		if is_on_floor():
+			$AnimatedSprite2D.play("Standing")
 
 	move_and_slide()
